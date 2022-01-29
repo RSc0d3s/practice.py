c2f4e577d4a8419606a33698b4067ba7b9351bae
Recca <98060592+RSc0d3s@users.noreply.github.com>
2022-01-29 20:51:20 +0900

    Create buildaTranslator.py

---
+++ buildaTranslator.py
@@ -0,0 +1,20 @@
+#Giraffe Language
+#vowels -> g all vowels becomes g
+# dog -> dgg since o=vowel
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            if letter.isupper():
+                translation = translation + "G"
+
+            else:
+                translation = translation + "g"
+
+        else:
+            translation = translation + letter
+
+    return translation
+
+print (translate(input("Enter a phrase: ")))
