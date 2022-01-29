7273d99800c6e24a0142131cf8aeb49a553f1249
Recca <98060592+RSc0d3s@users.noreply.github.com>
2022-01-29 19:53:26 +0900

    Create whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+# use to get  program to keep program running until conditions are true
+i = 1
+while i <= 10: #keep looping while condition is true
+    print (i)
+    i = i + 1 #shorthand i += 1
+
+print("Done wih loop.")
+
+p = 1
+while p <= 1000:
+    print (p)
+    p = p + 1 #shorthand i += 1
+
+print("Done wih loop.")
