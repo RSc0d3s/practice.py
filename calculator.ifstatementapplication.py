636faee05d3cb19422de015e0cf4669307feb032
Recca <98060592+RSc0d3s@users.noreply.github.com>
2022-01-29 19:34:44 +0900

    Create calculator.ifstatementapplication.py

---
+++ calculator.ifstatementapplication.py
@@ -0,0 +1,22 @@
+#real operating calculator
+#if statement application
+
+#variables
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (e.g. : +, -, / or * : ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1+num2)
+
+elif op == "-":
+    print(num1 - num2)
+
+elif op == "/":
+    print(num1 / num2)
+
+elif op == "*":
+    print(num1 / num2)
+
+else:
+    print("Invalid operator. ")
